81e662039681c983cdddb61ba7392d8ca6830ae8
Jorge Sánchez Escobedo <jorge.sanchez.escobedo@gmail.com>
2026-07-03 23:12:38 -0600

    added ipify

--- apps/ddns/cmd/main.go
+++ apps/ddns/cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/jorgesanchez-e/localenvironment/apps/ddns/internal/infra/ipgetter"
 	"github.com/jorgesanchez-e/localenvironment/config"
 )
 
@@ -18,5 +20,18 @@ func main() {
 		log.Fatalf("Failed to get simple DDNS config: %v", err)
 	}
 
-	fmt.Println(simpleDDNS)
+	fmt.Println("Simple DDNS config:", simpleDDNS)
+
+	ipGetter := ipgetter.NewIPGetter()
+	ipv4, err := ipGetter.GetIPV4(context.Background())
+	if err != nil {
+		log.Printf("ipgetter error %v", err)
+	}
+	ipv6, err := ipGetter.GetIPV6(context.Background())
+	if err != nil {
+		log.Printf("ipgetter error %v", err)
+	}
+
+	fmt.Println("IPv4:", ipv4)
+	fmt.Println("IPv6:", ipv6)
 }
